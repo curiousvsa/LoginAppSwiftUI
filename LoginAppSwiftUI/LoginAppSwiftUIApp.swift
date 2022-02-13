c427cb26e5d2a23b8dba72e3fb88e3b5b30b9183
vsa15 <valencioaraujo15@gmail.com>
2022-02-13 19:14:49 +0530

    Initial Commit

---
+++ LoginAppSwiftUI/LoginAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginAppSwiftUIApp.swift
+//  LoginAppSwiftUI
+//
+//  Created by Valencio Araujo on 13/02/22.
+//
+
+import SwiftUI
+
+@main
+struct LoginAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
